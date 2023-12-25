d033dee52a9bce36a53eaa579eece7917e2050ea
Ivan Arkaev <75647984+ha308ing@users.noreply.github.com>
2023-12-25 15:24:14 +0400

    update node.js email with .env

--- node.js/09-email/index.js
+++ node.js/09-email/index.js
@@ -1,16 +1,19 @@
 const nodemailer = require("nodemailer");
+const { EMAIL_SERVICE, EMAIL_USER, EMAIL_PASSWORD, EMAIL_RECIPIENT } = process.env;
 
 const transporter = nodemailer.createTransport({
-    service: "<service: yandex, gmail>",
+    // service: "gmail",
+    service: EMAIL_SERVICE,
     auth: {
-        user: "<login>",
-        pass: "<password>",
+        user: EMAIL_USER,
+        pass: EMAIL_PASSWORD,
     },
 });
 
 const mailOptions = {
-    from: "<from-email>",
-    to: "<to-email-1>, <to-email-2>",
+    from: EMAIL_USER,
+    // provide multiple in a string with commas
+    to: `${EMAIL_RECIPIENT}`,
     subject: "Sending email using node.js",
     html: "<h1>Hello!</h1>",
     // text: "Hello!",
